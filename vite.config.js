fc38eeef43f60c32516718d81161871be6f31520
Christian Almli <chrisalm97@gmail.com>
2024-12-09 14:47:19 +0100

    simplify security config, let _headers file handle it

--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,6 @@ import svgLoader from 'vite-svg-loader'
 export default defineConfig({
   plugins: [vue(), svgLoader()],
   base: '/',
-    server: {
-      headers: {
-        'Content-Security-Policy': `
-        default-src 'self';
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com;
-        style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-        font-src 'self' https://fonts.gstatic.com;
-        img-src 'self' data: blob: https:;
-        connect-src 'self' https:;
-        frame-src 'self';
-        worker-src 'self' blob:;
-      `.replace(/\s+/g, ' ').trim()
-      }
-    },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -33,6 +19,7 @@ export default defineConfig({
       '@fortawesome/free-solid-svg-icons',
       '@fortawesome/free-brands-svg-icons',
       '@fortawesome/vue-fontawesome',
+      'gsap',
       'aos',
       'three'
     ]
